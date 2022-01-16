124413c1af7d5c808fe26ea63d1e095e60ff80c8
gjianbo <gjianbo@qq.com>
2022-01-16 10:05:27 +0800

    测试

--- pkg/setting/section.go
+++ pkg/setting/section.go
@@ -10,11 +10,15 @@ type ServetSettingS struct {
 }
 
 type AppSettingS struct {
-	DefaultPageSize int
-	MaxPageSize     int
-	LogSavePath     string
-	LogFileName     string
-	LogFileExt      string
+	DefaultPageSize      int
+	MaxPageSize          int
+	LogSavePath          string
+	LogFileName          string
+	LogFileExt           string
+	UploadSavePath       string
+	UploadServerUrl      string
+	UploadImageMaxSize   int
+	UploadImageAllowExts []string
 }
 
 type DatabaseSettings struct {
